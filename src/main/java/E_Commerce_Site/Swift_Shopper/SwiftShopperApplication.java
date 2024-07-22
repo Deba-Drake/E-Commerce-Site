3e3e1518ea717dee66dc8d07cc1e549134ec973c
Deba-Drake <debayandas.185@gmail.com>
2024-07-22 21:52:08 +0530

    Initial commit

---
+++ src/main/java/E_Commerce_Site/Swift_Shopper/SwiftShopperApplication.java
@@ -0,0 +1,14 @@
+package E_Commerce_Site.Swift_Shopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftShopperApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SwiftShopperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/E_Commerce_Site/Swift_Shopper/SwiftShopperApplicationTests.java
@@ -0,0 +1,13 @@
+package E_Commerce_Site.Swift_Shopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftShopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
